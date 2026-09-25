335b114dc43bee1e95d41aa32f79097a65d3deb1
aayush-letzryd <vendor_aayush@letzryd.com>
2026-09-25 14:33:25 +0530

    feat(schedule): Set lock interval to 170m and schedule to 5 AM and 8 AM IST

--- src/runner.py
+++ src/runner.py
@@ -42,7 +42,7 @@ def is_another_execution_running(conn, current_run_id):
                 SELECT run_id, created_at 
                 FROM uber_pipeline_execution_logs 
                 WHERE status = 'RUNNING' 
-                  AND created_at > (NOW() - INTERVAL '115 minutes')
+                  AND created_at > (NOW() - INTERVAL '170 minutes')
                   AND run_id != %s;
             """, (current_run_id,))
             row = cur.fetchone()
